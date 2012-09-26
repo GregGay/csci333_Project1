38a54ed1715028b4d5e6dc64491c12a9f2f70533
greggay <greggay@debian.globalsuite.net>
2012-09-26 00:30:34 -0400

    queue tester to see if enqueue and dequeue are working.

---
+++ src/LLQueue/queue_tester.cpp
@@ -0,0 +1,20 @@
+#include "Queue.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+	Queue* queue=new Queue();
+	queue->enqueue(5);
+	queue->enqueue(10);
+	queue->enqueue(15);
+
+	cout << queue->dequeue() << endl;
+	cout << queue->dequeue() << endl;
+	cout << queue->dequeue() << endl;
+
+	delete queue;
+
+	return 0;
+}
