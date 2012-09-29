eb167070ceca20069eb7d0401568e6d74f8b330e
GregGay <ggay@unca.edu>
2012-09-28 23:09:56 -0400

    Unit test for Array Queue

---
+++ test/AQueue.cpp
@@ -0,0 +1,80 @@
+#include "gtest/gtest.h"
+#include "../src/AQueue/Queue.h"
+
+TEST(AQueueTest, Initialization) {
+	Queue* q = new Queue();
+	EXPECT_EQ(0, q->size());
+	delete q;
+}
+
+TEST(AQueueTest, Enqueue) {
+	Queue* q=new Queue();
+	q->enqueue(1);
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+
+	EXPECT_EQ(4, q->size());
+	delete q;
+}
+
+TEST(AQueueTest, EnqueueForLoop) {
+	Queue* q=new Queue();
+	
+	for (int i=0; i<23; ++i) {
+		q->enqueue(i);
+	}
+
+	EXPECT_EQ(23, q->size());
+	delete q;
+}
+
+TEST(AQueueTest, Dequeue) {
+	Queue* q=new Queue();
+	q->enqueue(1);
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+
+	q->dequeue();
+	q->dequeue();
+	q->dequeue();
+	q->dequeue();
+
+	EXPECT_EQ(0, q->size());
+	delete q;
+}
+
+TEST(AQueueTest, Dequeue2) {
+	Queue* q=new Queue();
+	q->enqueue(1);
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+
+	EXPECT_EQ(q->dequeue(), 1);
+	delete q;
+}
+
+TEST(AQueueTest, isEmpty) {
+	Queue* q=new Queue();
+	
+	EXPECT_EQ(true, q->isEmpty());
+	delete q;
+}
+
+TEST(AQueueTest, isEmpty2) {
+	Queue* q=new Queue();
+	q->enqueue(1);
+	q->enqueue(2);
+	q->enqueue(3);
+	q->enqueue(4);
+
+	q->dequeue();
+	q->dequeue();
+	q->dequeue();
+	q->dequeue();
+
+	EXPECT_EQ(true, q->isEmpty());
+	delete q;
+}
