c790a68f079689869b13f859db127204fb17edb4
GregGay <ggay@unca.edu>
2012-09-28 22:52:55 -0400

    List Queue header file

--- src/LQueue/Queue.h
+++ src/LQueue/Queue.h
@@ -1,18 +1,13 @@
 #ifndef __QUEUE_H__
 #define __QUEUE_H__
-
-#include "Node.h"
+#include <list>
 
 class Queue {
   private:
-	Node* front;
-	Node* back;
-	int numElements;
-
+	std::list<int> LQueue;
 
   public:
 	Queue();
-	~Queue();
 	void enqueue(int value);
 	int dequeue();
 	bool isEmpty();
