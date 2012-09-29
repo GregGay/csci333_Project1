827415a2b5f974bbe6c4222c2365401a1f379c18
GregGay <ggay@unca.edu>
2012-09-28 23:19:46 -0400

    Time Test for Array Queue

---
+++ src/TimingTest/ATime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+#include "../AQueue/Queue.h"
+
+int main() {
+	int start, end, elapsed;
+	double seconds;
+	Queue* queue=new Queue();
+	start = clock();
+	
+	for (long i=0; i<9999999; ++i) {
+		queue->enqueue(i);
+	}
+
+	for (long i=0; i<9999999; ++i) {
+		queue->dequeue();
+	}
+
+	end = clock();
+	elapsed = end - start;
+	seconds = (double) elapsed/CLOCKS_PER_SEC;
+
+	std::cout << "Elapsed time for AQueue is: " << seconds << " seconds " << std::endl;
+	delete queue;
+	return 0;
+}
